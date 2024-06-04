67e27167002b741a5c45f752b188e0d992408ad1
ABHAY <abhaychadha15@gmail.co>
2024-06-04 09:51:23 -0400

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,3 @@
-// import '@/app/ui/global.css';
-// import { Inter, Lusitana } from 'next/font/google';
-// // import { inter } from '@/app/ui/fonts';
-// export const inter = Inter({ subsets: ['latin'] });
-//  export const lusitana = Lusitana({
-//   weight: ['400', '700'],
-//   subsets: ['latin'],
-// });
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang="en">
-//       <body className={`${inter.className} antialiased`}>{children}</body>
-//     </html>
-//   );
-// }
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
  
