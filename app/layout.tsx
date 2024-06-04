50bef4023750a313be8f5d98b711e9929f3715ca
ABHAY <abhaychadha15@gmail.co>
2024-06-04 09:40:58 -0400

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
- 
+import { Inter, Lusitana } from 'next/font/google';
+// import { inter } from '@/app/ui/fonts';
+export const inter = Inter({ subsets: ['latin'] });
+ export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
 export default function RootLayout({
   children,
 }: {
